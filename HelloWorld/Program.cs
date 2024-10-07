763001e8c500efcd465a911312675757ab33bb9b
KorshunovCareeria <158146367+KorshunovCareeria@users.noreply.github.com>
2024-10-07 12:02:13 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,32 @@
     {
         public static void Main(string[] args)
         {
-           
+           {
+            // Pyydetään käyttäjää syöttämään merkkijono
+            Console.WriteLine("Anna merkkijono, jossa on kirjaimia ja numeroita: ");
+            string syote = Console.ReadLine();
+
+            // Lasketaan kuinka monta lukua ja kirjainta syötteessä on
+            int kirjainMaara = 0;
+            int numeroMaara = 0;
+
+            foreach (char merkki in syote)
+            {
+                if (char.IsLetter(merkki))
+                {
+                    kirjainMaara++;
+                }
+                else if (char.IsDigit(merkki))
+                {
+                    numeroMaara++;
+                }
+            }
+
+            // Tulostetaan tulokset
+            Console.WriteLine($"Kirjainten määrä: {kirjainMaara}");
+            Console.WriteLine($"Numeroiden määrä: {numeroMaara}");
         }
+
+
     }
 }
